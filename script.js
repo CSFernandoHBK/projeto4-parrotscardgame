b75b880aaa9e132974a9bd1cca6188dd24d9acf0
Fernando <fernandotam2@hotmail.com>
2022-08-20 16:34:49 -0300

    coments+logica

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-function RNI(a, b) {
-    return Math.floor(Math.random() * (b - a + 1)) + a
-  }
+//função de embaralhar
+function comparador() { 
+	return Math.random() - 0.5; 
+}
 
-const imagens = []
+//array com todas as imagens
+const imagens = ["img0", "img1", "img2", "img3", "img4", "img5", "img6"]
 
+//pergunta quantas cartas a pessoa quer
 let cond = true;
 let num = 0;
 while(cond){
@@ -15,25 +18,44 @@ while(cond){
     }
 }
 
+//gerar array de gifs selecionados
+
+let conj1 = [];
+for(let i=0; i<num/2; i++){
+    conj1[i] = imagens[i];
+}
+let conj2 = conj1;
+let conj = conj1.concat(conj2);
+
+//embaralha a array
+
+console.log(conj);
+conj.sort(comparador);
+console.log(conj);
+
+//cria o template de carta
 let carta = `<li class="carta">
-<div class="carta-verso" onclick="virarcarta(this)">
+<div class="carta-verso" onclick="virarcarta(this)">  
     <img src="./img/front.png">
 </div>
-<div class="carta-virada">
+<div class="imagem-frente escondido">
+    <img src="./img/img1.gif">
 </div>
 </li>`
 
+
+
+// coloca as cartas no código
 const ul = document.querySelector("ul");
 
 for(let i=0;i<num;i++){
     ul.innerHTML += carta;
 }
 
+// função de virar carta
 function virarcarta(card){
-    console.log(card);
-    //let n = RNI(0,6);
-    //imagem = document.querySelector(".carta-verso");
-    //console.log(imagem.innerHTML);
-    //imagem.innerHTML = `<img src="./img/img${n}.gif"></img>`
+    card.classList.toggle("carta-frente");
+    document.querySelector(".imagem-frente").classList.toggle("escondido");
+    document.querySelector
 }
 
